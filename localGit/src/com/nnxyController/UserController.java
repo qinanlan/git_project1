57e39a560505ab17b14d9b053a91f6d5a48c0105
qinanlan <74592137+qinanlan@users.noreply.github.com>
2020-12-01 19:59:50 +0800

    Update UserController.java

--- localGit/src/com/nnxyController/UserController.java
+++ localGit/src/com/nnxyController/UserController.java
@@ -1,7 +1,14 @@
 package com.nnxyController;
 
 public class UserController {
-	
+	[branch "master"] 
+	remote = origin 
+	merge = refs/heads/master
+[remote "origin"] 
+	url = https://github.com/huangbangyi2019/git_project.git 
+	fetch = +refs/heads/*:refs/remotes/origin/* 
+	push = refs/heads/master:refs/heads/master
+
 
         private int id;
 
